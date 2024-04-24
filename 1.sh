b4274f2e02c63b5e44e74ea2b720fd577db0db68
xcyxiner <593091948@qq.com>
2024-04-24 13:38:53 +0800

    还原依赖

---
+++ 1.sh
@@ -0,0 +1,7 @@
+npm install --save @nomiclabs/hardhat-waffle @nomiclabs/hardhat-web3
+npm install --save dotenv yesno
+npm install --save-dev "hardhat@^2.22.3"
+npm install --save-dev @openzeppelin/contracts @openzeppelin/contracts-upgradeable
+npm install --save-dev chai bn-chai chai-as-promised chai-bignumber chai-bn ethereum-waffle
+npm install --save-dev @nomiclabs/hardhat-ethers ethers@5
+npm install --save-dev @openzeppelin/hardhat-upgrades@1
